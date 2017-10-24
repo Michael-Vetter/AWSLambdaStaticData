39fa3e3131467c61a6c1bb5cf35ef408d7955ec1
Michael Vetter <michaelvetter@yahoo.com>
2017-10-24 10:16:11 -0400

    initial code change

--- AWSLambdaStaticData/Function.cs
+++ AWSLambdaStaticData/Function.cs
@@ -2,15 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Runtime.Serialization;
 using Amazon.Lambda.Core;
+using System.IO;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace AWSLambdaStaticData
 {
-    public class Function
+    public class GigawadData
     {
         
         /// <summary>
@@ -19,9 +21,25 @@ namespace AWSLambdaStaticData
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public string Get(ILambdaContext context)
         {
-            return input?.ToUpper();
+            GigawadStaticData myData = new GigawadStaticData();
+
+            myData.name = "Gigawad from Lambda";
+            myData.message = "Hooray for Lambda!";
+
+            return JsonConvert.SerializeObject(myData);
         }
     }
+
+    [DataContract]
+    public class GigawadStaticData
+    {
+        [DataMember]
+        internal string name;
+
+        [DataMember]
+        internal string message;
+    }
+
 }
